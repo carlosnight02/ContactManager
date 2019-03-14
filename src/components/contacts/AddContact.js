ff7406672e0b962e7d798fb01b5f6500b86c6dde
Carlos Murillo <carlos.murillo@cgi.com>
2019-03-14 17:08:34 -0400

    Error Checking & Display

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -4,76 +4,97 @@ import uuid from 'uuid';
 import TextInputGroup from '../layout/TextInputGroup';
  
 class AddContact extends Component {
-  state = {
-      name: '',
-      email: '',
-      phone: ''
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        errors: {}
     };
 
-  onSubmit = (dispatch,e) => {
-      e.preventDefault();
-      //console.log(this.state);
-      const {name, mail, phone} = this.state;
-      const newContact = {
-        id:uuid(),
-        name,
-        mail,
-        phone
-      }
-      dispatch({type: 'ADD_CONTACT', payload: newContact});
+    onSubmit = (dispatch,e) => {
+        e.preventDefault();
+        //console.log(this.state);
+        const {name, mail, phone} = this.state;
+
+        // Check For Errors
+        if(name ===''){
+            this.setState({errors: {name: 'Name is requiered'}})
+            return;
+        }
+        
+        if(mail === ''){
+            this.setState({errors:{email: 'Email is required'}});
+            return;
+        }
+     
+        if(phone === ''){
+            this.setState({errors:{phone: 'Phone is required'}});
+            return;
+        }
+      
+        const newContact = {
+            id:uuid(),
+            name,
+            mail,
+            phone
+        }
+        dispatch({type: 'ADD_CONTACT', payload: newContact});
 
       //Clear State
-      this.setState({
-        name: '',
-        email:'',
-        phone: ''
-
+        this.setState({
+            name: '',
+            email:'',
+            phone: '',
+            errors: {}
       });
-  }  
-  onChange = (e) => this.setState({[e.target.name]: e.target.value});
+    }  
+    onChange = (e) => this.setState({[e.target.name]: e.target.value});
    
-  render() {
-    const { name, email, phone }= this.state;
-    return (
-    <Consumer>
-        {value =>{
-            const { dispatch } = value;
-            return(
-                <div className="card mb-3">
-                    <div className="card-header">Add Contact</div> 
-                    <div className="card-body">
-                        <form onSubmit={this.onSubmit.bind(this, dispatch)} >
-                            <TextInputGroup
-                                label="Name"
-                                name="name"                                
-                                placeholder="Enter Name.."
-                                value= {name}
-                                onChange= {this.onChange}
-                            />
-                             <TextInputGroup
-                                label="Email"
-                                name="email"
-                                type="email"
-                                placeholder="Enter Email.."
-                                value= {email}
-                                onChange= {this.onChange}
-                            />
-                             <TextInputGroup
-                                label="Phone"
-                                name="phone"                                
-                                placeholder="Enter Phone.."
-                                value= {phone}
-                                onChange= {this.onChange}
-                            />       
-                            <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
-                        </form>
+    render() {
+        const { name, email, phone, errors }= this.state;
+        return (
+        <Consumer>
+            {value =>{
+                const { dispatch } = value;
+                return(
+                    <div className="card mb-3">
+                        <div className="card-header">Add Contact</div> 
+                        <div className="card-body">
+                            <form onSubmit={this.onSubmit.bind(this, dispatch)} >
+                                <TextInputGroup
+                                    label="Name"
+                                    name="name"                                
+                                    placeholder="Enter Name.."
+                                    value= {name}
+                                    onChange= {this.onChange}
+                                    error={errors.name}
+                                />
+                                <TextInputGroup
+                                    label="Email"
+                                    name="email"
+                                    type="email"
+                                    placeholder="Enter Email.."
+                                    value= {email}
+                                    onChange= {this.onChange}
+                                    error={errors.mail}
+                                />
+                                <TextInputGroup
+                                    label="Phone"
+                                    name="phone"                                
+                                    placeholder="Enter Phone.."
+                                    value= {phone}
+                                    onChange= {this.onChange}
+                                    error={errors.phone}
+                                />       
+                                <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            );
-        }}
-    </Consumer>
+                );
+            }}
+        </Consumer>
     
-   );
+    );
  }
 }
 export default AddContact;
--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const TextInputGroup = ({
     label,
@@ -7,7 +8,8 @@ const TextInputGroup = ({
     value,
     placeholder,
     type,
-    onChange
+    onChange,
+    error
 
 }) => {
     return (
@@ -15,12 +17,27 @@ const TextInputGroup = ({
             <label htmlFor={name}>{label}</label>
             <input 
                 type={type}
-                name={name}                             
-                className="form-control form-control-lg" 
+                name={name}  
+                
+                /*classname va prendre en premier les classes que nous voulons appliquer
+                - deuxième paramètre va être un objet qui va avoir le nom de classe que 
+                nous voulons is-invalid dans ce cas*/
+                className={classnames('form-control form-control-lg', 
+                {
+                    'is-invalid': error
+                })}
                 placeholder={placeholder}
                 value= {value}
                 onChange= {onChange}            
-            />                    
+            /> 
+            {/*invalid-feedback est lancé si is-invalid est true*/}
+            {
+                error && <div className="invalid-feedback">
+                {
+                    error
+                }</div>
+            }
+                           
         </div>      
     );
 };
@@ -31,7 +48,8 @@ TextInputGroup.propTypes = {
     value: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired
+    label: PropTypes.string.isRequired,
+    error: PropTypes.string
 }
 
 TextInputGroup.defaultProps = {
